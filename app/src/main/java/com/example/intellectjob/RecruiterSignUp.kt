a73fd24572e50cee52cc70718aa6cd5628ed4bdf
estiakahamed01 <tasin220116@diit.edu.bd>
2026-07-08 19:28:30 +0600

    Added : Recruiter Sign Up Page Validation

--- app/src/main/java/com/example/intellectjob/RecruiterSignUp.kt
+++ app/src/main/java/com/example/intellectjob/RecruiterSignUp.kt
@@ -2,8 +2,10 @@ package com.example.intellectjob
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.intellectjob.databinding.ActivityRecruiterSignUpBinding
@@ -17,14 +19,40 @@ class RecruiterSignUp : AppCompatActivity() {
         binding = ActivityRecruiterSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCreateRecruiter.setOnClickListener {
-            val intent = Intent(this, RecruiterHomePage::class.java)
-            startActivity(intent)
-        }
-
         binding.tvSignIn.setOnClickListener {
             val intent = Intent(this, SignIn::class.java)
             startActivity(intent)
+        }
+
+        binding.btnCreateRecruiter.setOnClickListener {
+            val companyName = binding.etCompanyName.text.toString().trim()
+            val recruiterName = binding.etRecruiterName.text.toString().trim()
+            val email = binding.etEmail.text.toString().trim()
+            val phone = binding.etPhone.text.toString().trim()
+            val website = binding.etWebsite.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val confirmPassword = binding.etConfirmPassword.text.toString().trim()
+            val passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{6,}$".toRegex()
+
+
+            if (companyName.isEmpty() || recruiterName.isEmpty() || email.isEmpty() || website.isEmpty() || phone.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Please complete your details", Toast.LENGTH_SHORT).show()
+            } else {
+                if (!password.matches(passwordRegex)) {
+                    Toast.makeText(this, "Password requires: 6+ chars, 1 uppercase, 1 lowercase, 1 number, and 1 special char", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    if (password != confirmPassword) {
+                        Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(this, "Account Registered!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, RecruiterHomePage::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+
         }
 
     }
