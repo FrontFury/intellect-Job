b623103ce499c563815e76be781bf578ca37ac83
estiakahamed01 <tasin220116@diit.edu.bd>
2026-07-08 19:40:32 +0600

    Added : Redirect Recruiter Home Page To Create Job

--- app/src/main/java/com/example/intellectjob/RecruiterHomePage.kt
+++ app/src/main/java/com/example/intellectjob/RecruiterHomePage.kt
@@ -1,15 +1,24 @@
 package com.example.intellectjob
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intellectjob.databinding.ActivityRecruiterHomePageBinding
 
 class RecruiterHomePage : AppCompatActivity() {
+    private  lateinit var binding: ActivityRecruiterHomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recruiter_home_page)
+        binding = ActivityRecruiterHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvCreateJob.setOnClickListener {
+            val intent = Intent(this@RecruiterHomePage, CreateJob::class.java)
+            startActivity(intent)
+        }
     }
 }
